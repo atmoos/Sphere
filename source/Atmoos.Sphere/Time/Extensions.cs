70040c537d9e2724c3e1aed8582b691e2135640a
Thomas Kägi <kaegit@gmail.com>
2024-05-13 00:48:36 +0200

    Add timer stream.

---
+++ source/Atmoos.Sphere/Time/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+using static System.Threading.Tasks.ConfigureAwaitOptions;
+
+namespace Atmoos.Sphere.Time;
+
+public static class Extensions
+{
+    public static IAsyncEnumerable<TimeSpan> AsStream(this TimeSpan interval, CancellationToken token = default)
+    {
+        return Stream(interval > TimeSpan.Zero ? interval : throw new ArgumentOutOfRangeException(nameof(interval), interval, "Interval strictly greater zero required"), token);
+
+        static async IAsyncEnumerable<TimeSpan> Stream(TimeSpan interval, [EnumeratorCancellation] CancellationToken token)
+        {
+            TimeSpan next = TimeSpan.Zero;
+            Stopwatch timer = Stopwatch.StartNew();
+
+            while (!token.IsCancellationRequested) {
+                yield return next;
+                var elapsed = timer.Elapsed;
+                next += interval * (1 + (Int32)((elapsed - next) / interval));
+                await Task.Delay(next - elapsed, token).ConfigureAwait(None);
+            }
+        }
+    }
+}
