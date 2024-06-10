fd8f1d6ff0459c0fba9f293dcdf3c7ca81c63038
Thomas Kägi <kaegit@gmail.com>
2024-06-10 22:38:36 +0200

    Delete Maybe. C# has a null aware type system.

--- source/Atmoos.Sphere.Functional/Extensions.cs
+++ source/Atmoos.Sphere.Functional/Extensions.cs
@@ -2,13 +2,8 @@ namespace Atmoos.Sphere.Functional;
 
 public static class Extensions
 {
-    public static Maybe<T> Join<T>(this Maybe<Maybe<T>> composite)
-        where T : notnull => composite.SelectMany(m => m);
     public static Result<T> Join<T>(this Result<Result<T>> composite)
         where T : notnull => composite.SelectMany(m => m);
-    public static Maybe<TResult> SelectMany<T, U, TResult>(this Maybe<T> value, Func<T, Maybe<U>> selector, Func<T, U, TResult> join)
-        where T : notnull where U : notnull where TResult : notnull
-            => value.SelectMany(t => selector(t).Select(u => join(t, u)));
     public static Result<TResult> SelectMany<T, U, TResult>(this Result<T> value, Func<T, Result<U>> selector, Func<T, U, TResult> join)
         where T : notnull where U : notnull where TResult : notnull
             => value.SelectMany(t => selector(t).Select(u => join(t, u)));
