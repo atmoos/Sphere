9728f0a027b695de0044172245714991e1662311
Thomas Kägi <kaegit@gmail.com>
2026-08-27 21:37:27 +0200

    Add allocation free convenience type

---
+++ source/Atmoos.Sphere/Memory/AllocationFree.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Atmoos.Sphere.Memory;
+
+public static class AllocationFree<TItem>
+    where TItem : class, new()
+{
+    private static readonly TItem item = new();
+    public static ref readonly TItem Item => ref item;
+}
+
+public static class AllocationFree<TReference, TItem>
+    where TReference : class
+    where TItem : class, TReference, new()
+{
+    private static readonly TReference item = new TItem();
+    public static ref readonly TReference Item => ref item;
+}
